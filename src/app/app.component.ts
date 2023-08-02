2c8f26f5359f2e4c52efa91ac50a8381e26cd3d9
lina <marcecaroc@gmail.com>
2023-08-02 16:15:56 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  //ToDo: eliminar esta lista al momento de integrar la API//
+  filter: "all" | "active" | "done" = "all"
+
+  allItems = [
+    {description: "dormir", done:true},
+    {description: "ir a mercar", done:false},
+    {description: "lavar la ropa", done:false},
+    {description: "ir al doctor", done:false},
+    {description: "ir al odontologo", done:false},
+
+  ]
+
+  get items() {
+    if (this.filter === "all"){
+      return this.allItems
+    }
+    return this.allItems.filter((item)=>{
+      this.filter ==="done" ? item.done :!item.done
+    })
+  }
+
+
+  addItem(description:string){
+    this.allItems.unshift({
+      description,
+      done:false
+    })
+  }
+
+  removeItem(item:Item){
+    this.allItems.splice(this.allItems.indexOf(item),1)
+  }
+
 }
